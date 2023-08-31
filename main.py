b265a96f18c37272a899851bf9d92ff4daf550a2
Kramer Campbell <kramer@kramerc.com>
2023-08-31 07:22:31 -0700

    Run sub-Python processes under env and update gitignore

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+import signal
 import subprocess
+import sys
+
 import toml
 
 if __name__ == '__main__':
@@ -10,8 +13,16 @@ if __name__ == '__main__':
     if os.path.exists(config["database_path"]["path"].replace("{USER_HOME}", user_home_dir)):
         os.remove(config["database_path"]["path"].replace("{USER_HOME}", user_home_dir))
 
-    process = subprocess.Popen(['python', os.path.join('db_manager.py')])
-    process.wait()
-    process = subprocess.Popen(['python', os.path.join('file_watcher.py')])
+    db_process = subprocess.Popen([sys.executable, os.path.join('db_manager.py')])
+    db_process.wait()
+
+    file_process = None
+    try:
+        file_process = subprocess.Popen([sys.executable, os.path.join('file_watcher.py')])
+        file_process.wait()
+    except KeyboardInterrupt:
+        # Handle Ctrl+C gracefully
+        file_process.send_signal(signal.SIGINT)
+
     # commented out because it doesn't work properly right now.
-    # subprocess.call(['python', os.path.join('gui.py')])
+    # subprocess.call([sys.executable, os.path.join('gui.py')])
