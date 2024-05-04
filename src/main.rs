8a05e78dd5f6cdef3c6663e024272c347b77a055
UR91K <judeefanner@gmail.com>
2024-05-05 01:35:25 +0200

    Added LiveSet implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::collections::HashSet;
-use std::path;
+use std::fmt;
+use std::fs::File;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+
 use chrono::{DateTime, Utc};
-use uuid::Uuid;
+use xmltree::Element;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Id(u64);
@@ -17,6 +21,12 @@ struct AbletonVersion {
     major: u32,
     minor: u32,
     patch: u32,
+}
+
+impl fmt::Display for AbletonVersion {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Ableton {}.{}.{}", self.major, self.minor, self.patch)
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash)]
@@ -98,24 +108,21 @@ struct Plugin {
 struct Sample {
     id: Id,
     name: String,
-    path: path::PathBuf,
+    path: PathBuf,
     is_present: bool
 }
 
 #[derive(Debug)]
 struct LiveSet {
-    uuid: Uuid,
-    identifier: u64,
-    path: path::PathBuf,
+    id: Id,
+    path: PathBuf,
     file_hash: String,
     last_scan_timestamp: DateTime<Utc>,
-    name: String,
+    file_name: String,
     creation_time: DateTime<Utc>,
     last_modification_time: DateTime<Utc>,
     creator: String,
     key_signature: KeySignature,
-    major_version: u32,
-    minor_version: u32,
     ableton_version: AbletonVersion,
     tempo: f32,
     time_signature: TimeSignature,
@@ -123,61 +130,70 @@ struct LiveSet {
     furthest_bar: u32,
     plugins: HashSet<Id>,
     samples: HashSet<Id>,
+    _xml_root: Option<Element>, // Private field to hold XML data
 }
 
+impl LiveSet {
+    fn new(path: PathBuf) -> Result<Self, String> {
+        let mut live_set = LiveSet {
+            id: Id::new(),
+            path,
+            file_hash: String::new(),
+            last_scan_timestamp: Utc::now(),
+            file_name: String::new(),
+            creation_time: Utc::now(),
+            last_modification_time: Utc::now(),
+            creator: String::new(),
+            key_signature: KeySignature::default(),
+            ableton_version: AbletonVersion::default(),
+            tempo: 0.0,
+            time_signature: TimeSignature::default(),
+            estimated_duration: chrono::Duration::zero(),
+            furthest_bar: 0,
+            plugins: HashSet::new(),
+            samples: HashSet::new(),
+            _xml_root: None,
+        };
+
+        // Attempt to load XML data
+        match live_set.load_xml_data() {
+            Ok(_) => Ok(live_set), // Return the initialized LiveSet if XML data loading succeeds
+            Err(err) => Err(err), // Return an error if XML data loading fails
+        }
+    }
+
+    fn load_xml_data(&mut self) -> Result<(), String> {
+        let path = Path::new(&self.path);
+        if !path.exists() || !path.is_file() || path.extension().unwrap_or_default() != "als" {
+            return Err(format!("{}: is not a valid Ableton Live Set file", self.file_name));
+        }
+
+        if self.last_modification_time.is_none() || self.creation_time.is_none() {
+            eprintln!("{}: Missing file times", self.file_name);
+            self.update_file_times();
+        }
+
+        let mut file = match File::open(&path) {
+            Ok(file) => file,
+            Err(err) => return Err(format!("{}: Failed to open file {}: {}", self.file_name, path.display(), err)),
+        };
+
+        let mut data = Vec::new();
+        if let Err(err) = file.read_to_end(&mut data) {
+            return Err(format!("{}: Failed to read file {}: {}", self.file_name, path.display(), err));
+        }
+
+        match Element::parse(data.as_slice()) {
+            Ok(root) => {
+                self._xml_root = Some(root);
+                Ok(())
+            }
+            Err(err) => Err(format!("{}: {} is not a valid XML file: {}", self.file_name, path.display(), err)),
+        }
+    }
+}
+
+
 fn main() {
-    // dummy data
-    let uuid = Uuid::new_v4();
-    let identifier = 19;
-    let path = path::PathBuf::from("C:/Users/user/Documents/Projects/abstract/acid 2 Project/acid 2.als");
-    let file_hash = "dummy_hash".to_string();
-    let last_scan_timestamp = Utc::now();
-    let name = "acid 2".to_string();
-    let creation_time = Utc::now();
-    let last_modification_time = Utc::now();
-    let creator = "Ableton Live 11.0.0".to_string();
-    let key_signature = KeySignature {
-        tonic: Tonic::C,
-        scale: Scale::Major,
-    };
-    let major_version = 11;
-    let minor_version = 0;
-    let ableton_version = AbletonVersion {
-        major: 11,
-        minor: 0,
-        patch: 0,
-    };
-    let tempo = 120.0;
-    let time_signature = TimeSignature {
-        numerator: 4,
-        denominator: 4,
-    };
-    let estimated_duration = chrono::Duration::minutes(5);
-    let furthest_bar = 32;
-    let plugins: HashSet<Id> = HashSet::new();
-    let samples: HashSet<Id> = HashSet::new();
 
-    let live_set = LiveSet {
-        uuid,
-        identifier,
-        path,
-        file_hash,
-        last_scan_timestamp,
-        name,
-        creation_time,
-        last_modification_time,
-        creator,
-        key_signature,
-        major_version,
-        minor_version,
-        ableton_version,
-        tempo,
-        time_signature,
-        estimated_duration,
-        furthest_bar,
-        plugins,
-        samples,
-    };
-
-    println!("{:#?}", live_set);
 }
