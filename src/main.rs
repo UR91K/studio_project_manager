57d42e24737c1e7195879109e4137c75e25364ca
UR91K <judeefanner@gmail.com>
2024-05-09 16:51:38 +0200

    Moved helpers, types and errors to separate files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod custom_types;
 mod errors;
+mod helpers;
 
 use custom_types::{Id,
                    TimeSignature,
@@ -9,7 +10,8 @@ use custom_types::{Id,
                    KeySignature,
                    PluginFormat,
                    Plugin,
-                   Sample
+                   Sample,
+                   XmlTag,
 };
 
 use std::collections::{HashMap, HashSet};
@@ -32,229 +34,20 @@ use log::LevelFilter;
 use env_logger::Builder;
 use thiserror::Error;
 use crate::errors::{LiveSetError, TimeSignatureError};
-
-
-fn validate_time_signature(value: i32) -> Result<i32, TimeSignatureError> {
-    if value >= 0 && value <= 16777215 {
-        Ok(value)
-    } else {
-        Err(TimeSignatureError::InvalidEncodedValue(value))
-    }
-}
-
-// This is the value of an EnumEvent tag that the time signature is stored in as a weirdly encoded number
-const TIME_SIGNATURE_ENUM_EVENT: i32 = -63072000;
-
-fn format_file_size(size: u64) -> String {
-    const KB: u64 = 1024;
-    const MB: u64 = 1024 * KB;
-    const GB: u64 = 1024 * MB;
-
-    if size < KB {
-        format!("{} B", size)
-    } else if size < MB {
-        format!("{:.2} KB", size as f64 / KB as f64)
-    } else if size < GB {
-        format!("{:.2} MB", size as f64 / MB as f64)
-    } else {
-        format!("{:.2} GB", size as f64 / GB as f64)
-    }
-}
-
-fn extract_gzipped_data(file_path: &Path) -> Result<Vec<u8>, String> {
-    let mut file = match File::open(&file_path) {
-        Ok(file) => file,
-        Err(err) => return Err(format!("Failed to open file {}: {}", file_path.display(), err)),
-    };
-
-    let mut gzip_decoder = GzDecoder::new(&mut file);
-    let mut decompressed_data = Vec::new();
-    if let Err(err) = gzip_decoder.read_to_end(&mut decompressed_data) {
-        return Err(format!("Failed to decompress file {}: {}", file_path.display(), err));
-    }
-
-    Ok(decompressed_data)
-}
-
-fn parse_xml_data(xml_data: &[u8], file_name: &Option<String>, file_path: &Path) -> Result<Element, String> {
-    let xml_data_str = match std::str::from_utf8(xml_data) {
-        Ok(s) => s,
-        Err(err) => return Err(format!("{:?}: Failed to convert decompressed data to UTF-8 string: {}", file_name, err)),
-    };
-
-    let xml_start = match xml_data_str.find("<?xml") {
-        Some(start) => start,
-        None => return Err(format!("{:?}: No XML data found in decompressed file", file_name)),
-    };
-
-    let xml_slice = &xml_data_str[xml_start..];
-
-    let start_time_xml = Instant::now();
-    let root = match Element::from_reader(Cursor::new(xml_slice.as_bytes())) {
-        Ok(root) => root,
-        Err(err) => return Err(format!("{:?}: {} is not a valid XML file: {}", file_name, file_path.display(), err)),
-    };
-    let duration = start_time_xml.elapsed();
-    println!("Creating XML Element: {:.2?}", duration);
-
-    Ok(root)
-}
-
-#[derive(Debug, Clone)]
-struct XmlTag {
-    name: String,
-    attributes: Vec<(String, String)>,
-}
-
-/// Retrieves all the empty tags which are immediate children of the search queries
-fn find_tags(xml_data: &[u8], search_queries: &[&str]) -> HashMap<String, Vec<Vec<XmlTag>>> {
-    let mut reader = Reader::from_reader(xml_data);
-    reader.trim_text(true);
-
-    let mut buf = Vec::new();
-    let mut all_tags: HashMap<String, Vec<Vec<XmlTag>>> = HashMap::new();
-    let mut current_tags: HashMap<String, Vec<XmlTag>> = HashMap::new();
-
-    let mut in_target_tag = false;
-    let mut depth: u8 = 0;
-    let mut current_query = String::new();
-
-    loop {
-        match reader.read_event_into(&mut buf) {
-            Ok(Event::Start(ref event)) => {
-                let name = std::str::from_utf8(event.name().as_ref()).unwrap().to_string();
-                if search_queries.contains(&name.as_str()) {
-                    in_target_tag = true;
-                    depth = 0;
-                    current_query = name;
-                    current_tags.entry(current_query.clone()).or_default();
-                } else if in_target_tag {
-                    depth += 1;
-                }
-            }
-            Ok(Event::Empty(ref event)) => {
-                if in_target_tag && depth == 0 {
-                    let name = std::str::from_utf8(event.name().as_ref()).unwrap().to_string();
-                    let mut attributes = Vec::new();
-                    for attr in event.attributes() {
-                        let attr = attr.unwrap();
-                        let key = std::str::from_utf8(attr.key.as_ref()).unwrap().to_string();
-                        let value = std::str::from_utf8(attr.value.as_ref()).unwrap().to_string();
-                        attributes.push((key, value));
-                    }
-                    current_tags.get_mut(&current_query).unwrap().push(XmlTag {
-                        name,
-                        attributes,
-                    });
-                }
-            }
-            Ok(Event::End(ref event)) => {
-                let name = std::str::from_utf8(event.name().as_ref()).unwrap().to_string();
-                if name == current_query {
-                    in_target_tag = false;
-                    all_tags.entry(current_query.clone()).or_default().push(current_tags[&current_query].clone());
-                    current_tags.get_mut(&current_query).unwrap().clear();
-                } else if in_target_tag {
-                    depth -= 1;
-                }
-            }
-            Ok(Event::Eof) => {
-                break;
-            }
-            _ => (),
-        }
-        buf.clear();
-    }
-    all_tags
-}
-
-fn find_attribute(tags: &[XmlTag], tag_query: &str, attribute_query: &str) -> Option<String> {
-    // println!("Searching for attribute '{}' in tag '{}'", attribute_query, tag_query);
-    for tag in tags {
-        if tag.name == tag_query {
-            for (key, value) in &tag.attributes {
-                if key == attribute_query {
-                    // println!("Found attribute '{}' with value: {}", attribute_query, value);
-                    return Some(value.clone());
-                }
-            }
-        }
-    }
-    // println!("Attribute '{}' not found in tag '{}'", attribute_query, tag_query);
-    None
-}
-
-fn find_empty_event(xml_data: &[u8], search_query: &str) -> Option<HashMap<String, String>> {
-    debug!("Searching for empty event with query: {}", search_query);
-    trace!("XML data: {:?}", std::str::from_utf8(xml_data));
-
-    let mut reader = Reader::from_reader(xml_data);
-    reader.trim_text(true);
-
-    let mut buf = Vec::new();
-
-    loop {
-        match reader.read_event_into(&mut buf) {
-            Ok(Event::Empty(ref event)) => {
-                let name = std::str::from_utf8(event.name().as_ref()).unwrap().to_string();
-                // debug!("Found empty event with name: {}", name);
-
-                if name == search_query {
-                    debug!("Empty event {} matches search query {}", name, search_query);
-
-                    let mut attributes = HashMap::new();
-                    for attr in event.attributes() {
-                        let attr = attr.unwrap();
-                        let key = std::str::from_utf8(attr.key.as_ref()).unwrap().to_string();
-                        let value = std::str::from_utf8(attr.value.as_ref()).unwrap().to_string();
-                        debug!("Found attribute: {} = {}", key, value);
-                        attributes.insert(key, value);
-                    }
-
-                    trace!("Attributes: {:?}", attributes);
-                    return Some(attributes);
-                }
-            }
-            Ok(Event::Eof) => {
-                debug!("Reached end of XML data");
-                break;
-            }
-            _ => (),
-        }
-        buf.clear();
-    }
-
-    debug!("Empty event {} not found", search_query);
-    None
-}
-
-fn find_all_plugins(xml_data: &[u8]) -> HashMap<String, Vec<String>> {
-    let search_queries = &["VstPluginInfo", "Vst3PluginInfo"];
-    let plugin_tags = find_tags(xml_data, search_queries);
-
-    let mut plugin_names: HashMap<String, Vec<String>> = HashMap::new();
-
-    for (query, tags_list) in plugin_tags {
-        let mut names = Vec::new();
-
-        for tags in tags_list {
-            let attribute_name = match query.as_str() {
-                "VstPluginInfo" => "PlugName",
-                "Vst3PluginInfo" => "Name",
-                _ => continue,
-            };
-
-            if let Some(name) = find_attribute(&tags, attribute_name, "Value") {
-                names.push(name);
-            }
-        }
-
-        plugin_names.insert(query, names);
-    }
-
-    plugin_names
-}
-
+use crate::helpers::{TIME_SIGNATURE_ENUM_EVENT,
+                     parse_encoded_value,
+                     validate_time_signature,
+                     format_file_size,
+                     extract_gzipped_data,
+                     parse_xml_data,
+                     find_tags,
+                     find_attribute,
+                     find_empty_event,
+                     find_all_plugins,
+                     get_file_timestamps,
+                     get_file_hash,
+                     get_file_name,
+};
 
 #[derive(Debug)]
 struct LiveSet {
@@ -279,65 +72,6 @@ struct LiveSet {
     vst3_plugin_names: Option<HashSet<String>>,
     sample_paths: Option<HashSet<Id>>,
 }
-
-fn get_file_timestamps(file_path: &PathBuf) -> Result<(DateTime<Local>, DateTime<Local>), String> {
-    let metadata = match fs::metadata(file_path) {
-        Ok(meta) => meta,
-        Err(error) => return Err(format!("Failed to retrieve file metadata: {}", error)),
-    };
-
-    let modified_time = match metadata.modified() {
-        Ok(time) => DateTime::<Local>::from(time),
-        Err(error) => return Err(format!("Failed to retrieve modified time: {}", error)),
-    };
-
-    let created_time = match metadata.created() {
-        Ok(time) => DateTime::<Local>::from(time),
-        Err(_) => Local::now(),
-    };
-
-    Ok((modified_time, created_time))
-}
-
-fn get_file_hash(file_path: &PathBuf) -> Result<String, String> {
-    let mut file = match File::open(file_path) {
-        Ok(file) => file,
-        Err(error) => return Err(format!("Failed to open file: {}", error)),
-    };
-
-    let mut hasher = Hasher::new();
-    let mut buffer = [0; 1024];
-
-    loop {
-        let bytes_read = match file.read(&mut buffer) {
-            Ok(bytes) => bytes,
-            Err(error) => return Err(format!("Failed to read file: {}", error)),
-        };
-
-        if bytes_read == 0 {
-            break;
-        }
-
-        hasher.update(&buffer[..bytes_read]);
-    }
-
-    let hash = hasher.finalize();
-    let hash_string = format!("{:08x}", hash);
-
-    Ok(hash_string)
-}
-
-pub fn get_file_name(file_path: &PathBuf) -> Result<String, String> {
-    match file_path.file_name() {
-        Some(file_name) => match file_name.to_str() {
-            Some(name) => Ok(name.to_string()),
-            None => Err("File name is not valid UTF-8".to_string()),
-        },
-        None => Err("File name is not present".to_string()),
-    }
-}
-
-
 
 impl LiveSet {
     fn new(file_path: PathBuf) -> Result<Self, String> {
@@ -389,7 +123,6 @@ impl LiveSet {
 
             vst2_plugin_names: None,
             vst3_plugin_names: None,
-
             sample_paths: None,
         };
 
@@ -446,43 +179,39 @@ impl LiveSet {
 
     fn update_time_signature(&mut self) -> Result<(), LiveSetError> {
         debug!("Updating time signature");
-
         trace!("XML data: {:?}", std::str::from_utf8(&self.xml_data));
 
         let search_query = "EnumEvent";
 
-        if let Some(attributes) = find_empty_event(&self.xml_data, search_query) {
-            debug!("Found time signature enum event");
-            trace!("Attributes: {:?}", attributes);
+        let event_attributes = find_empty_event(&self.xml_data, search_query)
+            .ok_or(LiveSetError::EnumEventNotFound)?;
 
-            if let Some(value) = attributes.get("Value") {
-                debug!("Found 'Value' attribute");
-                trace!("Value: {}", value);
+        debug!("Found time signature enum event");
+        trace!("Attributes: {:?}", event_attributes);
 
-                let encoded_value = value
-                    .parse::<i32>()
-                    .map_err(|e| LiveSetError::TimeSignatureError(TimeSignatureError::ParseEncodedError(e)))?;
-                debug!("Parsed encoded value: {}", encoded_value);
+        let value_attribute = event_attributes
+            .get("Value")
+            .ok_or(LiveSetError::ValueAttributeNotFound)?;
 
-                let time_signature = TimeSignature::from_encoded(encoded_value)
-                    .map_err(LiveSetError::TimeSignatureError)?;
-                debug!("Decoded time signature: {:?}", time_signature);
+        debug!("Found 'Value' attribute");
+        trace!("Value: {}", value_attribute);
 
-                self.time_signature = Some(time_signature);
-                info!(
-                "Time signature updated: {}/{}",
-                time_signature.numerator, time_signature.denominator
-            );
+        let encoded_value = parse_encoded_value(value_attribute)?;
+        debug!("Parsed encoded value: {}", encoded_value);
 
-                return Ok(());
-            } else {
-                error!("'Value' attribute not found");
-                return Err(LiveSetError::ValueAttributeNotFound);
-            }
-        } else {
-            error!("Time signature enum event not found");
-            return Err(LiveSetError::EnumEventNotFound);
-        }
+        let time_signature = TimeSignature::from_encoded(encoded_value)
+            .map_err(LiveSetError::TimeSignatureError)?;
+
+        debug!("Decoded time signature: {:?}", time_signature);
+
+        self.time_signature = Some(time_signature);
+        info!(
+            "Time signature updated: {}/{}",
+            self.time_signature.as_ref().unwrap().numerator,
+            self.time_signature.as_ref().unwrap().denominator
+        );
+
+        Ok(())
     }
 }
 
